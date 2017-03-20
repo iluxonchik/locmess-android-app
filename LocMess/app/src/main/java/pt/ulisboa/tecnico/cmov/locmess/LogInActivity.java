44aa9390eab88147e1a1e2109740a163f3e950d2
hulevych <valentyn-1@hotmail.com>
2017-03-20 02:33:47 +0000

    first 3 Activities done

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/LogInActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/LogInActivity.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.cmov.locmess;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void register(View v) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void logIn(View v) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/SignUpActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/SignUpActivity.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.cmov.locmess;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void logIn(View v) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
