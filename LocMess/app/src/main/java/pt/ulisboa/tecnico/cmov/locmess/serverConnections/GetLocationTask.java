67f342406a358dd8c0b8c1cca73bc1a052cf09eb
Roberto Ponte <ponte.roberto.cordeiro@gmail.com>
2017-04-10 22:51:07 +0000

    Working on location.

---
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/serverConnections/GetLocationTask.java
@@ -0,0 +1,23 @@
+package pt.ulisboa.tecnico.cmov.locmess.serverConnections;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Roberto Ponte on 4/10/2017.
+ */
+
+public class GetLocationTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        //TODO: Get The location from the server
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        //TODO: Update interface 
+        super.onPostExecute(s);
+
+    }
+}
